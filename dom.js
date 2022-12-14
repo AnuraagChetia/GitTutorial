062b6dbfefb1d1df64a76fa54a3b0efebb8e3b09
AnuraagChetia <111113882+AnuraagChetia@users.noreply.github.com>
2022-12-15 00:02:42 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,5 @@
+const title = document.getElementById('header-title');
+title.style.borderBottom = 'solid 3px #000'
+const addItem = document.getElementsByClassName('title');
+addItem[0].style.fontWeight = 'bold';
+addItem[0].style.color = 'green';
