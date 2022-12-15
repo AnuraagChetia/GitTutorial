edad1203c3a06e3d347d886be61ceed5a9674b08
AnuraagChetia <111113882+AnuraagChetia@users.noreply.github.com>
2022-12-15 12:27:51 +0530

    getElementByTagName

--- dom.js
+++ dom.js
@@ -1,5 +1,6 @@
-const items = document.getElementsByClassName('list-group-item');
-items[2].style.backgroundColor = 'green';
-for (let i = 0; i< items.length; i++) {
-  items[i].style.fontWeight = 'bold';
+const li = document.getElementsByTagName('li');
+li[2].style.backgroundColor = 'red';
+for (let i = 0; i< li.length; i++) {
+  li[i].style.fontWeight = 'bold';
+  li[i].style.backgroundColor = 'grey';
 }
