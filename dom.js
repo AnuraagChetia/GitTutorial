fef29c9bcfd6ea7da0b0b14432d03b2528ca6192
AnuraagChetia <111113882+AnuraagChetia@users.noreply.github.com>
2022-12-15 12:59:30 +0530

    querySelectorAll
    also with the difference in querySelector and querySelectorAll

--- dom.js
+++ dom.js
@@ -1,4 +1,9 @@
-const secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.backgroundColor = 'green';
-const thirdItem = document.querySelector('.list-group-item:nth-child(3)');
-thirdItem.style.color = 'white';
+const items = document.querySelectorAll('.list-group-item');
+items[1].style.color = 'green';
+const odd = document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor = 'green';
+}
+
+
+querySelector selects only the first of its type as default whereas querySelectorAll selects all of its type as default
