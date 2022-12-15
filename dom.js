9f751c2dd2e93b8d67035c7047e4608af7d9e593
AnuraagChetia <111113882+AnuraagChetia@users.noreply.github.com>
2022-12-15 11:56:58 +0530

    getElementByClassName

--- dom.js
+++ dom.js
@@ -1,5 +1,5 @@
-const title = document.getElementById('header-title');
-title.style.borderBottom = 'solid 3px #000'
-const addItem = document.getElementsByClassName('title');
-addItem[0].style.fontWeight = 'bold';
-addItem[0].style.color = 'green';
+const items = document.getElementsByClassName('list-group-item');
+items[2].style.backgroundColor = 'green';
+for (let i = 0; i< items.length; i++) {
+  items[i].style.fontWeight = 'bold';
+}
