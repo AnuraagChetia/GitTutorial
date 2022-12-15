980ccca56cb60a8deaf3d8c3eb8893b92b737934
AnuraagChetia <111113882+AnuraagChetia@users.noreply.github.com>
2022-12-16 00:14:50 +0530

    Creating Nodes and Modifying Dom

--- dom.js
+++ dom.js
@@ -1,9 +1,75 @@
-const items = document.querySelectorAll('.list-group-item');
-items[1].style.color = 'green';
-const odd = document.querySelectorAll('li:nth-child(odd)');
-for(var i=0;i<odd.length;i++){
-    odd[i].style.backgroundColor = 'green';
-}
+//parentElement
+// const itemList = document.querySelector('#items');
+// itemList.parentElement.style.backgroundColor ='#f4f4f4'
+
+//firstChild
+// console.log(itemList.firstChild);
+
+//firstElementChild
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'Hello 1'
+
+//last child
+// console.log(itemList.lastChild);
+
+//lastElementChild
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'Hello 4';
+
+//nextSibling
+// console.log(itemList.nextSibling);
+
+//nextElementSibling
+// console.log(itemList.nextElementSibling);
+
+//previousSibling
+// console.log(itemList.previousSibling);
+
+//previousElementSibling
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.color = 'green';
+
+//createElement
+
+//Create a div
+// var newDiv = document.createElement('div');
+
+//Add Class
+// newDiv.className = 'hello';
+
+//Add id
+// newDiv.id = 'hey';
+
+//Add attribute
+// newDiv.setAttribute('title','hiiii')
+
+//Create a textNode
+// var newDivTextNode = document.createTextNode('Hello India');
+
+//Append to newDiv
+// newDiv.appendChild(newDivTextNode);
+
+// console.log(newDiv);
+
+// var container = document.querySelector('header .container');
+// var h1 = document.querySelector('header h1');
+// container.insertBefore(newDiv,h1);
+
+var helloDiv = document.createElement('div');
+var helloDivTextNode = document.createTextNode('Hello');
+helloDiv.appendChild(helloDivTextNode);
+
+var helloContainer = document.querySelector('header div');
+var h11 = document.querySelector('header h1');
+helloContainer.insertBefore(helloDiv,h11);
 
 
-querySelector selects only the first of its type as default whereas querySelectorAll selects all of its type as default
+var helloLi = document.createElement('li');
+helloLi.className = 'list-group-item';
+helloLi.appendChild(helloDivTextNode);
+
+var listContainer = document.querySelector('ul');
+console.log(listContainer)
+var li = document.querySelector('ul li');
+console.log(li)
+listContainer.insertBefore(helloLi,li);
